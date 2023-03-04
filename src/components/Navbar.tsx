6875d55317089765931fdac19ef3070d3b434009
Muhammad Qasim <m.qasim162000@gmail.com>
2023-03-04 19:36:12 +0500

    Navbar.tsx done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import propTypes from "prop-types";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Navbar(props) {
+type NavbarProps = {
+  colorPaletteHandler: (color: string) => void;
+  mode: string;
+  title: string;
+  toggleMode: () => void;
+  aboutText: string;
+};
+
+export default function Navbar(props: NavbarProps) {
   const colorGrey = () => {
     props.colorPaletteHandler("grey");
   };
@@ -21,9 +29,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="#">
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -38,15 +46,15 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <Link className="nav-link active" aria-current="page" to="#">
                 Home
-              </a>
+              </Link>
             </li>
-            {/* <li className="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="/about">
                 {props.aboutText}
               </Link>
-            </li> */}
+            </li>
           </ul>
           {/* Search field and button */}
           {/* <form className="d-flex" role="search">
